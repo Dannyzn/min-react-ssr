841b4fb8fa315e84e413e87f4bbe33a64c3526d5
Dannyzn <694518723@qq.com>
2022-02-19 16:00:43 +0800

    add-redux-reducers-file

---
+++ src/redux/reducers.js
@@ -0,0 +1,20 @@
+import { REQUEST_APPS,  RECEIVE_APPS } from './actions';
+
+function apps(state = {isFetching: false, apps: []}, action) {
+    switch(action.type) {
+        case REQUEST_APPS:
+            return Object.assign({}, state, {
+                isFetching: true
+            })
+        case RECEIVE_APPS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                apps: action.type
+            })
+        default: 
+            return false
+    }
+}
+
+
+export default apps
